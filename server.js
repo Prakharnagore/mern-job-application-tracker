a276d6b9da500420b8f9b29c597ade341515b6be
prakharnagore <prakhar.nagore@accessai.co>
2022-11-26 01:31:07 +0530

    deploy to vercel

--- server.js
+++ server.js
@@ -1,39 +1,39 @@
-import express from 'express';
+import express from "express";
 const app = express();
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 dotenv.config();
-import 'express-async-errors';
-import morgan from 'morgan';
+import "express-async-errors";
+import morgan from "morgan";
 
-import { dirname } from 'path';
-import { fileURLToPath } from 'url';
-import path from 'path';
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+import path from "path";
 
-import helmet from 'helmet';
-import xss from 'xss-clean';
-import mongoSanitize from 'express-mongo-sanitize';
-import cookieParser from 'cookie-parser';
+import helmet from "helmet";
+import xss from "xss-clean";
+import mongoSanitize from "express-mongo-sanitize";
+import cookieParser from "cookie-parser";
 // hello
 // db and authenticateUser
-import connectDB from './db/connect.js';
+import connectDB from "./db/connect.js";
 
 // routers
-import authRouter from './routes/authRoutes.js';
-import jobsRouter from './routes/jobsRoutes.js';
+import authRouter from "./routes/authRoutes.js";
+import jobsRouter from "./routes/jobsRoutes.js";
 
 // middleware
-import notFoundMiddleware from './middleware/not-found.js';
-import errorHandlerMiddleware from './middleware/error-handler.js';
-import authenticateUser from './middleware/auth.js';
+import notFoundMiddleware from "./middleware/not-found.js";
+import errorHandlerMiddleware from "./middleware/error-handler.js";
+import authenticateUser from "./middleware/auth.js";
 
-if (process.env.NODE_ENV !== 'production') {
-  app.use(morgan('dev'));
+if (process.env.NODE_ENV !== "production") {
+  app.use(morgan("dev"));
 }
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
 // only when ready to deploy
-app.use(express.static(path.resolve(__dirname, './client/build')));
+// app.use(express.static(path.resolve(__dirname, "./client/build")));
 
 app.use(express.json());
 app.use(helmet());
@@ -41,12 +41,13 @@ app.use(xss());
 app.use(mongoSanitize());
 app.use(cookieParser());
 
-app.use('/api/v1/auth', authRouter);
-app.use('/api/v1/jobs', authenticateUser, jobsRouter);
+app.use("/api/v1/auth", authRouter);
+app.use("/api/v1/jobs", authenticateUser, jobsRouter);
 
 // only when ready to deploy
-app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, './client/build', 'index.html'));
+app.get("/", (req, res) => {
+  app.use(express.static(path.resolve(__dirname, "client", "build")));
+  res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
 });
 
 app.use(notFoundMiddleware);
