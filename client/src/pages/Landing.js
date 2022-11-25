2d50a3461729e885a05b59807e6382e4f074abd2
prakharnagore <prakhar.nagore@accessai.co>
2022-11-26 01:21:57 +0530

    finalcode

---
+++ client/src/pages/Landing.js
@@ -0,0 +1,44 @@
+import main from "../assets/images/main.svg";
+import Wrapper from "../assets/wrappers/LandingPage";
+import { Logo } from "../components";
+import { Link } from "react-router-dom";
+import { Navigate } from "react-router-dom";
+import { useAppContext } from "../context/appContext";
+import React from "react";
+
+const Landing = () => {
+  const { user } = useAppContext();
+  return (
+    <React.Fragment>
+      {user && <Navigate to="/" />}
+      <Wrapper>
+        <nav>
+          <Logo />
+        </nav>
+        <div className="container page">
+          {/* info */}
+          <div className="info">
+            <h1>
+              job <span>tracking</span> app
+            </h1>
+            <p>
+              The smart way to keep track your job applications. Simple online
+              job tracking app. Track what's working for you, track job
+              application progress.
+              <br />
+              <small>
+                Made with ❤️ by <b>Prakhar Nagore </b>
+              </small>
+            </p>
+            <Link to="/register" className="btn btn-hero">
+              Login/Register
+            </Link>
+          </div>
+          <img src={main} alt="job hunt" className="img main-img" />
+        </div>
+      </Wrapper>
+    </React.Fragment>
+  );
+};
+
+export default Landing;
